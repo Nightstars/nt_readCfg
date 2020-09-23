669d18137178be9a54d8fa5c6ddd1c054ea295f2
Nightstars <ChristZhangowner@gmail..com>
2020-09-23 16:47:37 +0800

    添加项目文件。

---
+++ nt_readCfg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nt_readCfg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = ConfigurationManager.AppSettings["demo"].ToString();
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+    }
+}
